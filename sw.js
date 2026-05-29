4a606d4c335e0b1ee734c51cb145b65e9697628d
vengalink <vengalink@github.com>
2026-05-29 00:13:21 -0700

    Add service worker for auto cache update

---
+++ sw.js
@@ -0,0 +1,27 @@
+const VERSION = 'v3';
+const CACHE = 'timebox-' + VERSION;
+const FILES = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
